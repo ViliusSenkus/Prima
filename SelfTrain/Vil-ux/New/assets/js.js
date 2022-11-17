e86f187ccd1f6209008e8d22d2a8a09145f12acf
Vilius Senkus <vilius.s@outlook.com>
2022-11-17 15:02:47 +0200

    first push

---
+++ SelfTrain/Vil-ux/New/assets/js.js
@@ -0,0 +1,13 @@
+//tik pavyzdžiui kol iprasiu.
+
+//<button type="button" onclick="myFunction()">Click Me!</button>
+
+
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+}
+
+function naujaFunkcija(x) {
+    return x;
+}
