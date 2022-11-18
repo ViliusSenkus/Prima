b1a0abfcdd65af835e1a825d7291bd9804b67db3
Vilius Senkus <vilius.s@outlook.com>
2022-11-18 10:07:13 +0200

    experimenting with js

---
+++ SelfTrain/JS/PageGenerator.js
@@ -0,0 +1,9 @@
+function topSection(){
+    let top='<meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Auto generated page</title>';
+    document.getElementById("top").innerHTML = top;
+}
+function body(){
+    let body="some text here";
+    document.getElementById("body").innerHTML = body;
+
+}
