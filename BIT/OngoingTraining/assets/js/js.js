469f7063e639e3c2a9469c85c5499b4c85ee63bc
Vilius Senkus <vilius.s@outlook.com>
2023-01-06 00:00:08 +0200

    self implied project (partial calendar functions)

--- BIT/OngoingTraining/assets/js/js.js
+++ BIT/OngoingTraining/assets/js/js.js
@@ -41,8 +41,6 @@ function setTask (){} //Galbūt reikia įdėti į Task klasę
 
 const taskas= new Task('data', 'aprašymas', 'adresas');
 
-console.log();
-
 function form(){ 
         //Paima atliktos užduoties įvedimo formą. Atlieka senoviškai tą patį, ką ir:
         //let form = fetch("assets/htmls/form.txt").then(res => res.text()).then(res => console.log(res));
@@ -68,17 +66,28 @@ function setas(){
 
 const monthsEN = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const now = new Date();
-let month=now;
+const nowdate = new Date();
+let month = nowdate.getMonth();
+let year = nowdate.getFullYear();
+let monthDay = nowdate.getDate();
 
-console.log(now.toLocaleString());
+function constructMonth(){
+    let y=2023;
+    let m=0;
+    if (document.querySelector("#right").value="2"){
+        y="kažkas";
+        m="kažkas";
+    }
 
-function constructMonth(y, m){
-    m = m < 0  ? 11:m;
-    m = m > 11 ? 0:m;
-    monthName=monthsEN[m];
+    let dayOfWeek=findFirstDay(year, month)
+    let numberOfWeeks=findNumberOfWeeks(year, month);
+    let lastMonthDay=findLastMonthDay(year, month);
+    let dayCounter = 1;
+    
+    monthName=monthsEN[month];
+
     html=`<table>
-            <thead>Mėnesis</thead>
+            <thead>${monthName}</thead>
                 <tr>
                     <th>M</th>
                     <th>T</th>
@@ -87,18 +96,96 @@ function constructMonth(y, m){
                     <th>F</th>
                     <th>St</th>
                     <th>Sn</th>                            
-                </tr>
-                <tr>
-                    <td>d</td>
-                </tr>
-            </table>`;
+                </tr>`;
     
-    findMonth(y, m);
+    for (let i=1; i<=numberOfWeeks; i++){
+        html=html+'<tr>';
+        if (i==1){
+            for (let z=1; z<dayOfWeek; z++)
+            html=html+'<td></td>'
+            
+            for (let z=dayOfWeek; z<=7; z++){
+                html=html+`<td>${dayCounter}</td>`;
+                counter++;
+            }
+        }else{
+            for (let i=1; i<=7; i++){
+                if(counter>=lastMonthDay+1){
+                    html=html+'<td></td>'
+                }else{
+                    html=html+`<td>${dayCounter}</td>`;
+                    counter++;
+                }
+            }
+        }
+        html=html+'</tr>'
+    }
+    html=html+'</table>'
+    document.querySelector("#center").innerHTML=html;
 }
 
-function findMonth(y, m){
-    const requaredDateString = `${y}-${m}-01`;
-    const date=new Date(requaredDateString);
-    console.log(date.getFullYear(), monthsEN[m], date.getDay());
+function findFirstDay(){
+    const firstDay=new Date(year, month, 1, 12);
+    let firstDayOfWeek=firstDay.getDay();
+    if (firstDayOfWeek==0)
+        firstDayOfWeek=7;
+        console.log(firstDayOfWeek);
+    return firstDayOfWeek;
 }
-findMonth(2023,02)
+
+
+function findNumberOfWeeks(){
+    
+};
+
+function findLastMonthDay(){
+    let nextMonthDays=[29, 30, 31, 32];
+    nextMonthDays.map((i)=>{
+        let lastDay = new Date(nowdate).setDate(i);
+        lastDay=new Date(lastDay);
+        nextMonth=lastDay.getMonth();
+        console.log('nowdate', nowdate.getMonth(), ' nextMonth ', nextMonth, )
+        if(nextMonth!=nowdate.getMonth())
+            return i-1;
+    });
+};    
+
+// gal susikurti kalendoriaus klasę ir iš jos darytis 3 mėnsių objketus (nowMonth, prevMonth, nextMonth), o sukant kalendorių būtų galima tiesiog priskirti vienam objektui, kito parametrus.
+
+class Month{
+    constructor(){
+        
+    };
+
+    findLastMonthDay(){
+        let nextMonthDays=[29, 30, 31, 32];
+        nextMonthDays.map((i)=>{
+            let lastDay = new Date(nowdate).setDate(i);
+            lastDay=new Date(lastDay);
+            nextMonth=lastDay.getMonth();
+            console.log('nowdate', nowdate.getMonth(), ' nextMonth ', nextMonth, )
+            if(nextMonth!=nowdate.getMonth())
+                return i-1;
+        });
+    };
+
+    findFirstDay(){
+        const firstDay=new Date(year, month, 1, 12);
+        let firstDayOfWeek=firstDay.getDay();
+        if (firstDayOfWeek==0)
+            firstDayOfWeek=7;
+            console.log(firstDayOfWeek);
+        return firstDayOfWeek;
+    }
+    
+    findNumberOfWeeks(){
+        const numberOf1WeekDays=findFirstDay()-7;
+        const lastMonthDay=findLastMonthDay();
+        const weeks=(lastMonthDay-numberOf1WeekDays)/7
+        weeks = weeks%7==0? weeks
+        let counter=1;
+        for (i=2; i<=6; i++){
+
+        }
+    };
+}
