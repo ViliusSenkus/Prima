193a38c98df96e8b1187060fd4d41c949e490acd
Vilius Senkus <vilius.s@outlook.com>
2023-01-12 09:35:32 +0200

    keys fixed

--- BIT/REACT/2023-01-11/mini_blog/src/components/post/post.js
+++ BIT/REACT/2023-01-11/mini_blog/src/components/post/post.js
@@ -1,25 +1,23 @@
 import Blog from '../blog/blog';
 
 const blog = Blog.map( i => {
-    return (<>
-            <section className="post" key={i._id}>
-                <div key={i._id + "post"} className="wrapper">
-                    <img src={i.image} alt="" className="image" key={i._id + "img"}></img>
-                    <div className="category" key={i._id + "cat"}>{i.category}</div>
+    return (<section className="post" key={i._id}>
+                <div className="wrapper">
+                    <img src={i.image} alt="" className="image"></img>
+                    <div className="category">{i.category}</div>
                 </div>
 
-                <div className="date" key={i._id + "date"}>{i.date} / {i.comments}</div>
-                <div className="title" key={i._id + "title"}>{i.title}</div>
-                <div className="content" key={i._id + "cont"}>{i.content}...</div>
-                <div className="continue" key={i._id + "link"}>Continue reading →</div>
-            </section>
-            </>)
+                <div className="date">{i.date} / {i.comments}</div>
+                <div className="title">{i.title}</div>
+                <div className="content">{i.content}...</div>
+                <div className="continue">Continue reading →</div>
+            </section>)
 });
 
 
 const Post = () => {
     return(
-        <main key="mainkey">
+        <main>
                 {blog}
         </main>
     )
