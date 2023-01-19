f6f6bdfbed96c04be38921b4074ffb4f09c92162
Vilius Senkus <vilius.s@outlook.com>
2023-01-19 16:00:53 +0200

    advice app version 1.0 released

--- BIT/REACT/2023-01-18_advice/src/App.js
+++ BIT/REACT/2023-01-18_advice/src/App.js
@@ -4,57 +4,58 @@ import {useState, useEffect} from 'react'
 function App() {
 
   const API='https://api.adviceslip.com/advice';
-  const [data, setData] = useState({});           //atskiras įrašas
-  const [dataList, setDataList] = useState([]);   //sąrašas rodytų įrašų
+
+  const [data, setData] = useState({});
+  const [dataList, setDataList] = useState([]);
+  const [push, setPush] = useState(true);
 
   useEffect ( () => {
+
     fetch(API)
     .then((res) => res.json())
-    .then((res) => {setData(res.slip)})
-    if(localStorage.getItem('advices')!==null){
-        setDataList(JSON.parse(localStorage.getItem('advices')));
-        console.log(dataList);
+    .then((res) => {
+        setData(res.slip); 
+        
+        const toStorage = (object) => localStorage.setItem('advices', JSON.stringify(object));
+        const fromStorage = () =>  JSON.parse(localStorage.getItem('advices'));
+        
+        if(localStorage.getItem('advices')!=null){
+            let array = fromStorage();
+            let repeated = false;
+            
+            if ( array.length > 14 ){
+              localStorage.removeItem('advices')
+              array.splice(0,1);
+            }
+
+            array.filter(value => {
+                if (value.id===res.slip.id){
+                    value.count++;
+                    repeated=true;
+                }
+            })
+
+            if ( repeated === false ) {
+                array.push({ id:res.slip.id, name:res.slip.advice, count:0 });
+                toStorage(array);
+            }else{
+              toStorage(array);
+            }
+
+        }else{
+            toStorage([{ id:res.slip.id, name:res.slip.advice, count:0 }]);
         }
-  }, [])
 
-  const handleButton = (e) => {            //f-ja pasileidžia mygtuko paspaudimu
-    e.preventDefault();
-    // let tmp=[...dataList,{id:data.id, name:data.advice, count:0}];
-    // setDataList(tmp);
-    if (localStorage.getItem('advices')===null){    //pridedama pirma reikšmė
-      localStorage.setItem('advices', JSON.stringify([{id:data.id, name:data.advice, count:0}]))
-    } else{
-      const temp=JSON.parse(localStorage.getItem('advices')); //temp priima reiksmes is saugyklos
-      let n=0;                                 //reikalinga pasikarotjimų iškėlimui iš for ir if
-      for (let x of temp){
-          // let count=x.count;
-          if (x.id === data.id){
-           x.count++;
-          //  x.count=count;
-           n=x.count;
-          //  console.log(count)
-          }
-      }
-      if (n === 0){
-        temp=[...dataList,{id:data.id, name:data.advice, count:0}];
-        setDataList(temp);
-        localStorage.setItem('advices', JSON.stringify(dataList));
-      }else{            //esant atitikimų, pirmiausiai juose perduodame į dataList.
-        dataList=[...temp];
-        setDataList(dataList);
-        localStorage.setItem('advices', JSON.stringify(dataList));
-      }
-    }
-    fetch(API)
-    .then((res) => res.json())
-    .then((res) => {setData(res.slip)})
-  }
-
+        
+        setDataList(fromStorage());
+    });
+  },[push])
+  
   return (
     <div className="container">
       <h1>Advice of the Day</h1>
       <div>{data.advice}</div>
-      <button onClick={handleButton}>Give me one more Advice</button>
+      <button onClick={()=>setPush(!push)}>Give me one more Advice</button>
       <div>
         <h3>List of Advices recieved</h3>
           <table>
@@ -67,17 +68,19 @@ function App() {
                 </tr>
             </thead>
             <tbody>
-              {dataList.length > 0 && dataList.map((value, index) => {
-                  return (<tr key={index}>
-                            <td>{index+1}.</td>
-                            <td>{value.id}</td>
-                            <td>{value.name}</td>
-                            <td>{value.count}</td>
-                          </tr>)
-                })
+              {dataList?.map((value, index) => <tr key={index}>
+                                                  <td>{index+1}.</td>
+                                                  <td>{value.id}</td>
+                                                  <td>{value.name}</td>
+                                                  <td>{value.count}</td>
+                                                </tr>)
               }
             </tbody>
           </table>
+      </div>
+      <div>
+        <h3>Note:</h3>
+        <div>All viewed advice data is stored on your computer. To save computer resources and avoid crashes, the list will show the last 14/15 unique unrepeated items. Number of items depend on newly recieved advice.</div>
       </div>
     </div>
     
@@ -85,3 +88,5 @@ function App() {
 }
 
 export default App;
+
+
