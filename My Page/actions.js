0cf52c0d1cf3ed7a3e0ff753ba849dc9060c480c
Vilius Senkus <vilius.s@outlook.com>
2023-04-08 00:25:51 +0300

    navigation menu responsivenes and showing hiding fixed

--- My Page/actions.js
+++ My Page/actions.js
@@ -1,11 +1,20 @@
 const header = document.querySelector("header");
+const navigation_show = document.querySelectorAll("#navigation_show");
 const navigation = document.querySelector("nav");
 const show_hide_navigation = document.querySelector("#menu-spread-arrow");
+const hidden_up = document.querySelector(".hidden-up");
+const main = document.querySelector("main");
+console.log(main);
+let show_menu = true;
 
 show_hide_navigation.addEventListener("click", ()=>{
-      if(header.style.display=="none"){
-            header.style.display="flex"
+      if(!show_menu){
+            header.classList.remove("hidden-up");
+            main.classList.add("down");
+            show_menu = true;
       }else{
-            header.style.display="none"
+            header.classList.add("hidden-up");
+            main.classList.remove("down");
+            show_menu = false;
       }
 })
